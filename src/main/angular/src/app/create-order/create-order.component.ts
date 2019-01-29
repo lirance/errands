121de38ee6061e182564d745946e1a0d40462306
ZhiyuanHe1993 <hezhi@oregonstate.edu>
2019-01-29 12:30:57 -0800

    merge

---
+++ src/main/angular/src/app/create-order/create-order.component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {OrderService} from "../_services/order.service";
+import {first} from "rxjs/operators";
+
+@Component({
+  selector: 'app-create-order',
+  templateUrl: './create-order.component.html',
+  styleUrls: ['./create-order.component.css']
+})
+
+export class CreateOrderComponent implements OnInit {
+  orderForm: FormGroup;
+  loading = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private orderServie: OrderService) {
+  }
+
+  ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      orderid: [],
+      itemlist: ['', Validators.required],
+      storeadd: ['', Validators.required],
+      destination: [''],
+      state: ['', Validators.required],
+      timelimit: ['', Validators.required],
+      tip: ['', Validators.required],
+      maker: [],
+      recipient: []
+    });
+  }
+  onSubmit() {
+    this.loading = true;
+    this.orderForm.value.maker = localStorage.getItem('currentUserID');
+    this.orderServie.createOrder(this.orderForm.value).pipe(first()).subscribe(
+      success=>{
+        console.log('success!')
+        this.router.navigate(['/dashhome']);
+      }
+    )
+  }
+}
